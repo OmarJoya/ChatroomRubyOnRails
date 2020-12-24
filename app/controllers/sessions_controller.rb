189f5700afb9e286701c6c1e9602171177bcb922
Omar <ojoya@sciodev.com>
2020-12-24 13:55:12 -0600

    add login logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
+
   def new
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Login successfully"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Wrong login credentials"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path
   end
 end
